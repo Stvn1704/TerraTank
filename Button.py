47e3e39de7e5213f74c8f36d16d9fa705990bb18
cuajada69 <kilianvega22@gmail.com>
2024-11-21 12:59:21 -0500

    Update button

--- Button.py
+++ Button.py
@@ -12,6 +12,3 @@ class Buttons:
     def is_clicked(self, pos):
         # Comprobar si el botón fue clickeado o si el mouse esta encima del boton
         return self.rect.collidepoint(pos)
-
-boton_jugar = Buttons("Image/Start_BTN.png", 300, 270, 50, 50)
-boton_salir = Buttons("Image/Exit_BTN.png", 300, 350, 60, 60)
